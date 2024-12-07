266d78d4832ec59d1c310daf79f5c45c0defaa7a
Brahim-Mahfoudhi <Brahim.mahfoudhi@student.hogent.be>
2024-12-07 15:03:31 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
-    id 'com.github.jk1.dependency-license-report' version '2.9'
+    id("com.github.jk1.dependency-license-report") version "2.9"
     
 }
 
